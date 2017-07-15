330836f62d78ceb6d7940c331ce1d0f3f8ab8748
Thanh Ho <wowueh@gmail.com>
2017-07-15 14:32:14 +0700

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public static void InRaManHinh<T>(T any)
+        {
+            Console.WriteLine(any);
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            char b = 'C';
+            string c = "Ho Duc Nhat Thanh";
+            bool d = true;
+            float e = 50.45f;
+            decimal f = 45.56m;
+            InRaManHinh<int>(a);
+            InRaManHinh<char>(b);
+            InRaManHinh<string>(c);
+            Console.ReadKey();
+        }
+    }
+}
